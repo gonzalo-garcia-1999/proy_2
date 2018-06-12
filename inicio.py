1dae564d2a2ad0a8f3028fdc637ff40bcb541a99
gonzalo-garcia-1999 <40075812+gonzalo-garcia-1999@users.noreply.github.com>
2018-06-12 09:23:22 -0500

    Create inicio.py

---
+++ inicio.py
@@ -0,0 +1,19 @@
+#instrucciones
+def instrucciones():
+  print("Bienvenido")
+  print("\nEste programa le enseñara a usar python.")
+  print("\nInstrucciones:")
+  
+  lista_instrucciones=[
+  "Para comenzar inserte el numero '1'", 
+  "Al finalizar una lectura o ejercicio insertar 'continuar' o 'salir'",
+  "Si esta regresando al programa inserte el numero que esta al lado del tema en el que se quedo",
+  "Insertar todo en minusculas", 
+  "En un ejercicio insertar 'regresar' si desea retornar a la lectura"
+  ]
+  
+  for numero_de_instruccion in range(len(lista_instrucciones)):
+    instruccion_general=str(numero_de_instruccion+1)+"."+lista_instrucciones[numero_de_instruccion]
+    print(instruccion_general)
+
+instrucciones()

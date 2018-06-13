0577e101ab6699e2fd46bad423136fbea82dcd38
alexandrameerovici <39888680+alexandrameerovici@users.noreply.github.com>
2018-06-12 22:57:56 -0500

    Update proyecto_2_ifs.py

--- proyecto_2_ifs.py
+++ proyecto_2_ifs.py
@@ -1,1 +1,14 @@
-#hola
+def lectura_if_boolean():
+    print("Hola!")
+    print("En esta parte se te va a enseñar sobre las estructuras selectivas. Sin embargo, para entender que son las estructuras selectivas, se necesita entender que es un boolean. \n Una expresión booleana compara a dos factores y da una respuesta entre verdadero y falso dependiendo del resultado de la comparación.")
+    print(" Algunos ejemplos de expresiones booleanas: \n mayor: a>b \n mayor o igual: a>=b \n menor: a<b \n menor o igual: a<=b \n igual: a==b \n no igual: a!=b")
+    print("También, pueden haber expresiones booleanas que comparen mas de dos propiedades de distintos elementos. Por ejemplo:\n a<b and a<c \n a==b or a==c \n ")
+    print("Además, se pueden utilizar expresiones aritmeticas con los booleanss, tal y como:\n a+b>c+d \n a*b==b*c \n a%c!=a%b")
+    return
+lectura_if_boolean()
+def ejercicio_boolean():
+    print("Ahora, intetalo tu!")
+    print("Abajo, puedes encontrar diversos problemas con booleans, utiliza tu conocimiento y lo que acabas de entender para solucionarlo. \n Para contestar y escribir los caracteres solo has click en el espacioen blanco entre los dos numeros")
+    input1= input()
+    input2= input()
+    input3= input()
